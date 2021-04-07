f56b470705726581b9924031821a972f8824733e
“rafavg77” <rafavg77@gmail.com>
2021-04-06 23:58:59 -0500

    .

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,11 @@
 import os
 import logging
+import traceback
+import json
+import html
 from configparser import ConfigParser
-#from requests.models import Response
-from  telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from  telegram import ReplyKeyboardMarkup, ParseMode, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,CallbackContext
 from Utils.trackingMailAmericas import TrackApi
 from Utils.sendToTelegram import sendPhoto
 
@@ -67,7 +69,34 @@ def getTrackingStatus(update, context):
     status = TrackApi()
     lastStatus = status.accessTraking()
     logging.info("Sending: " + DOWNLOAD_PATH+lastStatus)
+    print(open(DOWNLOAD_PATH+lastStatus,'rb'))
     context.bot.send_photo(chat_id=update.message.chat_id,photo=open(DOWNLOAD_PATH+lastStatus,'rb'))
+
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )   
+
+    # Finally, send the message
+    context.bot.send_message(chat_id=bad_permission[0], text=message, parse_mode=ParseMode.HTML)
+
 
 if __name__ == '__main__':
 
@@ -78,6 +107,7 @@ if __name__ == '__main__':
     dispatcher.add_handler(CommandHandler('help', help))
     dispatcher.add_handler(CommandHandler('ping', ping))
     dispatcher.add_handler(MessageHandler(Filters.regex('^(Ping|Tracking Status)$'), reply))
+    dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
     updater.idle()
